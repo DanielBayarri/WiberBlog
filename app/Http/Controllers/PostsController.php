019b0ffb15b6af629a81582cf0445d7ad92cb4e7
Daniel  Bayarri <d.bayarri.b@gmail.com>
2023-07-10 21:26:38 +0200

    edit variable in controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -70,7 +70,6 @@ class PostsController extends Controller
     public function edit(int $id)
     {
         $post = Posts::find($id);
-
         return view('posts.edit', ['post' => $post]);
     }
 
